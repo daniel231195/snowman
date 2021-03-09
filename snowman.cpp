93a9065f5800be0a068fe2951ff1b35c1bc82339
daniel <dansela123@gmail.com>
2021-03-09 20:14:23 +0200

    commit 2

---
+++ snowman.cpp
@@ -0,0 +1,12 @@
+#include "snowman.hpp"
+#include <iostream>
+#include <stdexcept>
+#include <string>
+using namespace std;
+
+
+namespace ariel{
+    std::string snowman(int x){
+        return "_===_\n(.,.)\n( : )\n( : )";
+    }
+}
---
+++ snowman.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+
+namespace ariel
+{
+    std::string snowman(int x);
+} 
+// namespace ariel
